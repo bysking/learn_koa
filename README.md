1544913f73f6056855b4d75544e16657d9d69b45
chenpengxu <chenpengxu@dding.cn>
2019-10-15 14:32:23 +0800

    feat: async await解决异步

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # learn_koa
+1. 搭建基本的框架
+2. 监视入口文件 nodemon app.js
+
+
+		1.cd 到项目里面
+		2.npm init --yes   /   npm init  创建package.json
+		3.通过命令安装koa
+			 npm install koa --save
+			 cnpm install koa --save
+		4.引入koa使用
+			//引入Koa
+			const koa=require('koa');
+			const app=new koa();
+			//配置koa中间件
+			app.use( async (ctx)=>{
+			 ctx.body='hello koa2'
+			})
+			//监听端口
+			app.listen(3000);
+
+接下来，我们一起创建一个基本的 koa 应用
+
+async await 理解 async 声明一个函数异步的， await 是 async await 的简写 含义为等待一个异步方法执行完成
+
--- app.js
+++ app.js
@@ -7,3 +7,42 @@ app.use(async (ctx) => { // 配置中间件
 })
 
 app.listen(3000) // 监听端口
+
+async function getData(){ // async 返回一个promise对象
+  return '这是一个数据';
+}
+async function test() {
+  const d = await getData()
+  console.log(d)
+}
+
+// test() // 测试异步函数中等待异步函数执行
+
+async function getdate() {
+  console.log(2)
+  return '这是一个数据'
+}
+
+async function test2() {
+    console.log(1)
+    var d = await getdate()
+    console.log(3)
+    console.log(4)
+}
+
+// test2() // await 阻塞功能把异步改为同步
+
+function findData() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('后知后觉')
+    }, 1000)
+  })
+}
+
+async function test3() {
+  const v = await findData()
+  console.log(v)
+}
+
+test3() // 封装Promise 与async await一起使用
